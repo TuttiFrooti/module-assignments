ea8d12ea4ea37dcb5c6ff5e40a3c7033e18e8efa
Sam <samemami@me.com>
2020-02-05 08:48:03 +0100

    Ma3

---
+++ js1-ma3/sam-js1-ma3.js
@@ -0,0 +1,62 @@
+// Question 1
+(a, b) => {
+  return a - b
+}
+
+// Question 2
+fetch('https://elephant-api.herokuapp.com/elephants')
+.then(res => {
+  console.log(res);
+})
+.then(response => {
+  response.forEach(element => {
+    console.log(element.name)
+  });
+})
+.catch(() => {
+  document.location.href = "/error.html";
+})
+
+// Question 3
+let sentence = "These cats are outrageous.";
+sentence = sentence.replace(/cats/g, "giraffes");
+console.log(sentence)
+
+// Question 4
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+if (!params.has("userId")) {
+  document.location.href = "/third.html";
+} else if (params.get("userId") > 10) {
+  document.location.href = "/first.html";
+} else {
+  document.location.href = "/second.html";
+}
+
+// Question 5
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+
+container.removeChild(btn);
+
+// Question 6
+const animals = document.querySelector(".animals");
+const elephants = document.querySelector(".elephants");
+const new_li = document.createElement("li");
+new_li.className = "parrots";
+new_li.innerHTML = "Parrots";
+
+animals.appendChild(new_li);
+elephants.before(new_li);
+
+
+// Question 7
+fetch('https://api.rawg.io/api/games/3801')
+.then(res => res.json())
+.then(response => {
+  const rating_div = document.querySelector('.rating');
+  rating_div.innerHTML = response.rating;
+})
+.catch(error => {
+  console.log(error);
+})
